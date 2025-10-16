bba7ff9114c6702f75ae42284a8fdef3da2ec481
mproyyan <mproyyan@gmail.com>
2025-10-16 19:43:02 +0700

    create user service

---
+++ internal/services/user_service.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"github.com/google/uuid"
+	"github.com/mproyyan/user-api/internal/entities"
+)
+
+type UserService struct {
+	repo entities.UserRepository
+}
+
+func NewUserService(repo entities.UserRepository) *UserService {
+	return &UserService{repo}
+}
+
+func (s *UserService) GetAllUsers() ([]entities.User, error) {
+	return s.repo.FindAll()
+}
+
+func (s *UserService) GetUserByID(id string) (*entities.User, error) {
+	return s.repo.FindByID(id)
+}
+
+func (s *UserService) CreateUser(name, email string) (*entities.User, error) {
+	user := entities.User{
+		ID:    uuid.NewString(),
+		Name:  name,
+		Email: email,
+	}
+	return s.repo.Create(user)
+}
+
+func (s *UserService) DeleteUser(id string) error {
+	return s.repo.DeleteByID(id)
+}
